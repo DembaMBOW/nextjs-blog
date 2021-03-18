1f283181401de135eb1ec55e88c75686ee0e05db
DembaMBOW <demba.pro.76000@gmail.com>
2021-03-18 11:21:52 +0100

    initial commite

--- pages/index.js
+++ pages/index.js
@@ -12,11 +12,12 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Je suis Demba MBOW 20 ans étudiant  à la Nfactory School  à Rouen passioné par l'informatique </p>
         <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
+          (Ce tutoriel vous aidera à réaliser un petit Blog {' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+       
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
